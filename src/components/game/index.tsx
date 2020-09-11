a8f751b73475faf4dd90c393bd43fd4e14736ace
jonchan51 <32188705+jonchan51@users.noreply.github.com>
2020-09-11 23:38:34 +0800

    Add socketio dependency

--- src/components/game/index.tsx
+++ src/components/game/index.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import io from 'socket.io-client';
 import Home from './Home';
 
 const GameShell: React.FC = () => {
+  useEffect(() => {
+    const socket = io();
+
+    // TODO: setup error handlers for each event
+    // TODO: if there is a query pin, send in connection to join room immediately
+    // TODO: on reconnecting, display disconencted, reconnecting
+    // on reconnect, display reconnected
+    // on reconnect_failed, display fail, redirect to home
+    // and all other event handlers for the game events
+
+    return () => {
+      socket.close();
+    };
+  }, []);
   return <Home />;
 };
 
