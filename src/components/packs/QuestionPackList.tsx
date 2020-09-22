cf8f45b57d970aa5d926e74bc918229c0269d0fc
jonchan51 <32188705+jonchan51@users.noreply.github.com>
2020-09-23 02:50:46 +0800

    Use a drawer for filtering categories

--- src/components/packs/QuestionPackList.tsx
+++ src/components/packs/QuestionPackList.tsx
@@ -1,16 +1,28 @@
 import React, { useState, useEffect, useContext } from 'react';
 import {
   Box,
-  TextField,
   Accordion,
   AccordionActions,
   Button,
   AccordionSummary,
   Typography,
   AccordionDetails,
-  InputAdornment,
+  Paper,
+  InputBase,
+  IconButton,
+  SwipeableDrawer,
+  Divider,
+  ListItem,
+  Checkbox,
 } from '@material-ui/core';
-import { Edit, ExpandMore, Delete, Search } from '@material-ui/icons';
+import {
+  Edit,
+  ExpandMore,
+  Delete,
+  Search,
+  Filter,
+  FilterList,
+} from '@material-ui/icons';
 import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
 
 import {
@@ -61,13 +73,14 @@ const QuestionPackList: React.FC<Props> = ({
   );
   const [myPacks, setMyPacks] = useState(store.getLocalPacks());
   const [categories, setCategories] = useState([] as Category[]);
+  const [filterOpen, setFilterOpen] = useState(false);
+  const online = useOnlineStatus();
   const [search, setSearch] = useState<Filter>({
     name: '',
     categories: [],
-    tab: 'mine', // TODO: Maybe it would be good to see community first? only see mine first if in room
+    tab: online ? 'community' : 'mine',
   });
   const history = useHistory();
-  const online = useOnlineStatus();
 
   useEffect(() => {
     const setLocalCategories = () => setCategories(store.getCategories());
@@ -101,6 +114,8 @@ const QuestionPackList: React.FC<Props> = ({
     }
   };
 
+  console.log(myPacks);
+
   const handleDeletePack = (pack: LocalQuestionPack) => {
     // TODO use a modal to invoke this function
     if (name === null || !online) {
@@ -124,10 +139,63 @@ const QuestionPackList: React.FC<Props> = ({
     }
   };
 
+  const handleClearCategoryFilter = () => {
+    setSearch({ ...search, categories: [] });
+  };
+
   const filter = (pack: QuestionPackPostData) =>
     pack.name.includes(search.name) &&
     (search.categories.length === 0 ||
-      pack.categories.some(search.categories.includes));
+      pack.categories.some(category => search.categories.includes(category)));
+
+  const handleEditCategoryFilter = (category: string) => (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    let categories = search.categories;
+    if (event.target.checked) {
+      categories = [...categories, category];
+    } else {
+      categories = categories.filter(category1 => category1 !== category);
+    }
+    setSearch({ ...search, categories: categories });
+  };
+
+  // filter applies even without pressing apply
+  const filterDrawer = (
+    <SwipeableDrawer
+      anchor="left"
+      open={filterOpen}
+      onClose={() => setFilterOpen(false)}
+      onOpen={() => setFilterOpen(true)}
+    >
+      <Typography variant="h6">Filter by Categories</Typography>
+      <Divider />
+      {categories.sort().map(category => (
+        <ListItem key={category}>
+          <Checkbox
+            color="primary"
+            checked={search.categories.includes(category)}
+            value={category}
+            onChange={handleEditCategoryFilter(category)}
+            inputProps={{
+              'aria-label': `${category}-filter`,
+            }}
+          />
+          {category}
+        </ListItem>
+      ))}
+      <Button color="primary" onClick={handleClearCategoryFilter}>
+        Clear
+      </Button>
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={() => setFilterOpen(false)}
+      >
+        Close
+      </Button>
+    </SwipeableDrawer>
+  );
 
   // if in room, only use add to rooom button
   // if mine, allow for edit and delete
@@ -210,34 +278,36 @@ const QuestionPackList: React.FC<Props> = ({
   // or maybe if loading, shwo a skeleton instead
   return (
     <Box>
-      <TextField
-        placeholder="Search..."
-        value={search.name}
-        onChange={handleNameSearch}
-        fullWidth
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <Search />
-            </InputAdornment>
-          ),
-        }}
-      />
+      {filterDrawer}
       <ToggleButtonGroup
         exclusive
         value={search.tab}
         onChange={handleTabChange}
         aria-label="pack tab"
       >
-        <ToggleButton value="mine" aria-label="mine">
-          Mine
-        </ToggleButton>
         <ToggleButton value="community" aria-label="community">
-          Community
+          Community Packs
+        </ToggleButton>
+        <ToggleButton value="mine" aria-label="mine">
+          My Packs
         </ToggleButton>
       </ToggleButtonGroup>
-      {search.tab === 'mine' && ownedPackAccordions}
+      <Paper>
+        <Search />
+        <InputBase
+          placeholder="Search"
+          value={search.name}
+          onChange={handleNameSearch}
+          inputProps={{
+            'aria-label': 'search packs',
+          }}
+        />
+        <IconButton onClick={() => setFilterOpen(true)}>
+          <FilterList />
+        </IconButton>
+      </Paper>
       {search.tab === 'community' && communityPackAccordions}
+      {search.tab === 'mine' && ownedPackAccordions}
       {inRoom && <BackButton handleBack={handleBack!} />}
     </Box>
   );
