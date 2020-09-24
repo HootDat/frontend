7133c7367e601aceb8306d8cf7612f3f199bcfba
jonchan51 <32188705+jonchan51@users.noreply.github.com>
2020-09-24 23:18:48 +0800

    Imporve logged in elsewhere screen

--- src/components/game/LoggedInElsewhere.tsx
+++ src/components/game/LoggedInElsewhere.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import OuterGrid from '../common/OuterGrid';
 import CenteredInnerGrid from '../common/CenteredInnerGrid';
-import { Grid, Paper, Typography } from '@material-ui/core';
+import { Grid, Paper, Typography, makeStyles } from '@material-ui/core';
+import HootAvatar from '../common/HootAvatar';
+
+const useStyles = makeStyles(theme => ({
+  notice: {
+    padding: theme.spacing(2),
+  },
+}));
 
 const LoggedInElsewhere: React.FC = () => {
+  const classes = useStyles();
   return (
     <OuterGrid>
       <CenteredInnerGrid>
         <Grid item>
-          <Paper elevation={3}>
+          <Paper elevation={3} className={classes.notice}>
+            <HootAvatar size="large" number={1} />
             <Typography variant="body1">
-              Hoot Dat is open in another window. Use the the other window to
-              play instead.
+              Hoot Dat is open in another window. Use the other window to play
+              instead.
             </Typography>
           </Paper>
         </Grid>
